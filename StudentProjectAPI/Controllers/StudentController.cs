c0e53a8357a68b3f2f2775f86185de961c37d634
fares2718 <faresobaid2715@gmail.com>
2025-07-27 20:09:52 +0300

    Get All Students

---
+++ StudentProjectAPI/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentProjectAPI.DataSemulation;
+using StudentProjectAPI.Model;
+
+namespace StudentProjectAPI.Controllers
+{
+    [Route("api/Student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Student>> GetAllStudents ()
+        {
+            return Ok(StudentDataSemulation.StudentsList);
+        }
+
+    }
+}
---
+++ StudentProjectAPI/DataSemulation/StudentDataSemulation.cs
@@ -0,0 +1,15 @@
+﻿using StudentProjectAPI.Model;
+
+namespace StudentProjectAPI.DataSemulation
+{
+    public class StudentDataSemulation
+    {
+        public static readonly List<Student> StudentsList = new List<Student>()
+        {
+            new Student {Id=1,Name="Fares Obaid",Age=20,Grade=95},
+            new Student {Id=2,Name="Some Shit",Age=22,Grade=78},
+            new Student {Id=3,Name="Any Thing",Age=30,Grade=88},
+            new Student {Id=4,Name="Hassan Obaid",Age=25,Grade=93},
+        };
+    }
+}
