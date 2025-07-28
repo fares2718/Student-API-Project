f4606a9ed72ff8ed095ed9ddea80676935c11578
fares2718 <faresobaid2715@gmail.com>
2025-07-28 19:08:56 +0300

    add client side code

---
+++ Client-Side-Code/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Client_Side_Code
+{
+    internal class Program
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            httpClient.BaseAddress = new Uri("http://localhost:5151/api/Student");
+
+            await GetAllStudents();
+
+        }
+
+        static async Task GetAllStudents()
+        {
+            try
+            {
+                Console.WriteLine("\n__________________________\n");
+                Console.WriteLine("\nFetching all students....\n");
+                var students = await httpClient.GetFromJsonAsync<List<Student>>("Student");
+                if (students != null)
+                {
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine($"ID: {student.Id}, Name: {student.Name}, Age: {student.Age}, Grade: {student.Grade}");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+    }
+}
+
+
+
